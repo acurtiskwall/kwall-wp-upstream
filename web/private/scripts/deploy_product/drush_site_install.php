5c0927742283c9c27a8635430dd1c359e4fffaba
Kevin Wall <kevin@kwallcomany.com>
2022-05-11 13:53:19 -0700

    install first.

--- web/private/scripts/deploy_product/drush_site_install.php
+++ web/private/scripts/deploy_product/drush_site_install.php
@@ -10,13 +10,14 @@ $pass = generate_password_install();
 $email = 'site-deploys-aaaaeq6pviqdhxcpn7rglgn7h4@kwall.slack.com';
 
 print "Installing Wordpress...\n";
+passthru('wp core install --url=' . $url . ' --title=' . $title . ' --admin_user=' . $user . ' --admin_password=' . $pass . ' --admin_email=' . $email);
 passthru('wp db query < ' . $_ENV['DOCROOT'] . 'install/kwall-demo-site_dev_2022-05-10T17-47-37_UTC_database.sql');
 passthru("wp search-replace 'https://dev-kwall-demo-site.pantheonsite.io' '" . $secure_url . "'");
 passthru("wp user create " . $user . " 'info@kwallcompany.com' --role=admin --user_pass=" . $pass . "");
 //Copy files from template folder
 passthru("cp -r " . $_ENV['DOCROOT'] . "install/files_dev/ " . $_ENV['DOCROOT'] . "/wp-content/uploads/");
 
-//passthru('wp core install --url=' . $url . ' --title=' . $title . ' --admin_user=' . $user . ' --admin_password=' . $pass . ' --admin_email=' . $email);
+
 print "Install Wordpress complete.\n";
 
 $subject = $site_name . '.' . $env . " website admin credentials";;
