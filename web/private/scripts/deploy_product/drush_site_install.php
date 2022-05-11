5ff738b45e0d398e91a85818a0eaf2945a79f891
Kevin Wall <kevin@kwallcomany.com>
2022-05-11 09:25:34 -0700

    initial commit.

---
+++ web/private/scripts/deploy_product/drush_site_install.php
@@ -0,0 +1,29 @@
+<?php
+
+$env = $_ENV['PANTHEON_ENVIRONMENT'];
+$site_name = $_ENV['PANTHEON_SITE_NAME'];
+$url = 'http://' . $env . '-' . $site_name . '.pantheonsite.io';
+$title = 'Wordpress KWALL EDU';
+$user = $site_name . '-kwall';
+$pass = generate_password_install();
+$email = 'site-deploys-aaaaeq6pviqdhxcpn7rglgn7h4@kwall.slack.com';
+
+print "Installing Wordpress...\n";
+passthru('wp core install --url=' . $url . ' --title=' . $title . ' --admin_user=' . $user . ' --admin_password=' . $pass . ' --admin_email=' . $email);
+print "Install Wordpress complete.\n";
+
+$subject = $site_name . '.' . $env . " website admin credentials";;
+$message = "User: $user\nPassword: $pass";
+$acceptedForDelivery = mail($email, $subject, $message);
+
+function generate_password_install($length = 12){
+  $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+  $str = '';
+  $max = strlen($chars) - 1;
+
+  for ($i=0; $i < $length; $i++) {
+    $str .= $chars[mt_rand(0, $max)];
+  }
+
+  return $str;
+}
